b601106e01a592187e7f29625ca3f6e5386af4e9
Stephan <cicchelli.stephan@gmail.com>
2023-09-20 17:14:31 -0300

    req 4e5

--- src/Pages/pages/Login.tsx
+++ src/Pages/pages/Login.tsx
@@ -1,10 +1,70 @@
+import React, { useState } from 'react';
+
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [formValid, setformValid] = useState(false);
+
+  const handleEmailChange = (event: { target: { value: any; }; }) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    validateForm(newEmail, password);
+  };
+
+  const handlePasswordChange = (event: { target: { value: any; }; }) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    validateForm(email, newPassword);
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    if (formValid) {
+      localStorage.setItem('user', JSON.stringify({ email }));
+    }
+  };
+
+  const validateForm = (newEmail: string, newPassword: string | any[]) => {
+    const emailValid = /\S+@\S+\.\S+/.test(newEmail);
+    const passwordValid = newPassword.length > 6;
+
+    setformValid(emailValid && passwordValid);
+  };
+
   return (
-    <form>
-      <input type="email" data-testid="email-input" />
-      <input type="password" data-testid="password-input" />
-      <button data-testid="login-submit-btn">Enter</button>
-    </form>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ handleEmailChange }
+        />
+        <br />
+        <label htmlFor="password">Senha:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ handlePasswordChange }
+        />
+        <br />
+        <button
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={ !formValid }
+
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
   );
 }
 
