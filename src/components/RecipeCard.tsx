4a3dfa75b3d49303ba4d727cc877646196eb8cfd
marcus tourinho <viniciustourinho01@gmail.com>
2023-09-21 18:16:53 -0300

    Req 23, os requisitos que necessitam de testes irão apresentar falha

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { RecipeType } from '../Types';
 
 type RecipeCardProps = {
@@ -10,17 +11,23 @@ function RecipeCard({ recipe, index }: RecipeCardProps) {
   const recipeName = recipe.strMeal || recipe.strDrink;
   const recipeThumb = recipe.strMealThumb || recipe.strDrinkThumb;
 
+  const linkPath = recipe.strMeal
+    ? `/meals/${recipe.idMeal}`
+    : `/drinks/${recipe.idDrink}`;
+
   return (
-    <div data-testid={ `${index}-recipe-card` }>
-      <img
-        src={ recipeThumb }
-        alt={ recipeName }
-        data-testid={ `${index}-card-img` }
-      />
-      <span data-testid={ `${index}-card-name` }>
-        {recipeName}
-      </span>
-    </div>
+    <Link to={ linkPath } style={ { textDecoration: 'none', color: 'inherit' } }>
+      <div data-testid={ `${index}-recipe-card` }>
+        <img
+          src={ recipeThumb }
+          alt={ recipeName }
+          data-testid={ `${index}-card-img` }
+        />
+        <span data-testid={ `${index}-card-name` }>
+          {recipeName}
+        </span>
+      </div>
+    </Link>
   );
 }
 
