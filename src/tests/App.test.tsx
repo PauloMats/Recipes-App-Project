2a163e3cde49bff2147d072544949022d53bf37a
Gabriel <gabri.mevial@gmail.com>
2023-09-22 18:26:18 -0300

    adicionando teste App e Helper

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,32 +1,30 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from '../Pages/pages/Login';
+import renderWithRouter from '../Helper/renderWihtRouter';
 
-describe('Validação do login', () => {
-  const setup = () => {
-    const renderResult = render(<Login />);
-    const { getByTestId } = renderResult;
-    const emailInput = getByTestId('email-input');
-    const passwordInput = getByTestId('password-input');
-    const submitButton = getByTestId('login-submit-btn');
-    return { emailInput, passwordInput, submitButton };
-  };
+test('O formulário de login é renderizado corretamente', () => {
+  const { getByTestId, getByText } = renderWithRouter(<Login />);
 
-  test('O botão deve permanecer desativado se o e-mail for considerado inválido.', () => {
-    const { emailInput, submitButton } = setup();
-    fireEvent.change(emailInput, { target: { value: 'emailinvalido' } });
-    expect(submitButton).toBeDisabled();
-  });
+  const loginButton = getByTestId('login-submit-btn');
+  const emailInput = getByTestId('email-input');
+  const loginTexto = getByText('Login');
+  const passwordInput = getByTestId('password-input');
 
-  test('O botão deve permanecer desativado caso a senha tenha 6 caracteres ou menos.', () => {
-    const { passwordInput, submitButton } = setup();
-    fireEvent.change(passwordInput, { target: { value: '123456' } });
-    expect(submitButton).toBeDisabled();
-  });
+  expect(loginButton).toBeInTheDocument();
+  expect(emailInput).toBeInTheDocument();
+  expect(loginTexto).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+});
 
-  test('O botão deve estar em estado ativo quando o e-mail e a senha forem considerados válidos.', () => {
-    const { emailInput, passwordInput, submitButton } = setup();
-    fireEvent.change(emailInput, { target: { value: 'usuario@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'senha123456' } });
-    expect(submitButton).not.toBeDisabled();
-  });
+test('Validação de email e senha', async () => {
+  const { getByTestId } = renderWithRouter(<Login />);
+
+  const loginButton = getByTestId('login-submit-btn');
+  const emailInput = getByTestId('email-input');
+  const passwordInput = getByTestId('password-input');
+
+  await userEvent.type(emailInput, 'itsmenunes@gmail.com');
+  await userEvent.type(passwordInput, '1234567');
+  await userEvent.click(loginButton);
 });
