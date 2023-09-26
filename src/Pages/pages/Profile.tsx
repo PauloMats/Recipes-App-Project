32553b0cc95b48452703c7dfb64ee53779c93cfd
Stephan <cicchelli.stephan@gmail.com>
2023-09-26 17:48:15 -0300

    cria page profile  einicia teste

---
+++ src/Pages/pages/Profile.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+function Profile() {
+    const [userEmail, setUserEmail] = useState<string>('');
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const { email } = JSON.parse(storedUser);
+      if (email) setUserEmail(email);
+    }
+  }, []);
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+
+  const handleLogout = () => {
+    navigate('/');
+    localStorage.clear();
+  };
+
+  return (
+    <main>
+      <p data-testid="profile-email">{userEmail}</p>
+      <button
+        data-testid="profile-done-btn"
+        onClick={() => handleNavigation('/done-recipes')}
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={() => handleNavigation('/favorite-recipes')}
+        >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        onClick={handleLogout}
+        >
+        Logout
+      </button>
+    </main>
+  );
+}
+
+export default Profile;
