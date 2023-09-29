c301888541dda6243f3f9f7fea8f78470230dd14
PauloMats <pm.mats98@gmail.com>
2023-09-29 17:53:38 -0300

    Finalização dos testes do DoneRecipes

--- src/tests/DoneRecipes.test.tsx
+++ src/tests/DoneRecipes.test.tsx
@@ -48,3 +48,20 @@ describe('Testa a page DoneRecipes', () => {
     const linkCopied = screen.getAllByText('Link copied!');
     expect(linkCopied.length > 0).toBe(true);
   });
+
+  it('Verificando as funcionalidade dos filtros', async () => {
+    await renderWithRouter(<DoneRecipes />);
+    const buttonAll = screen.getByTestId('filter-by-all-btn');
+    const buttonMeals = screen.getByTestId('filter-by-meal-btn');
+    const buttonDrinks = screen.getByTestId('filter-by-drink-btn');
+    await userEvent.click(buttonMeals);
+    const HORIZONTAL_NAME_TEST_ID = '0-horizontal-name';
+    expect(screen.getAllByTestId(HORIZONTAL_NAME_TEST_ID)[0]).toBeInTheDocument();
+
+    await userEvent.click(buttonDrinks);
+    expect(screen.getAllByTestId(HORIZONTAL_NAME_TEST_ID)[0]).toBeInTheDocument();
+
+    await userEvent.click(buttonAll);
+    expect(screen.getAllByTestId(HORIZONTAL_NAME_TEST_ID)[0]).toBeInTheDocument();
+  });
+});
