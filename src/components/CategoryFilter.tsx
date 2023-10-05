60ea25bcc9a5101586788279ef3127dbc0f45e6b
brunoloubraz <brunolou.braz@gmail.com>
2023-10-05 18:09:54 -0300

    css finalizado

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -6,6 +6,12 @@ import goatIcon from '../images/goatIcon.svg';
 import Chicken from '../images/chicken.svg';
 import breakfast from '../images/breakfast.svg';
 import dessert from '../images/dessert.svg';
+import allDrinks from '../images/allDrinkIcon.svg';
+import shake from '../images/shake.svg';
+import other from '../images/other.svg';
+import cocoa from '../images/cocoa.svg';
+import ordinaryDrink from '../images/ordinaryDrink.svg';
+import cocktail from '../images/cocktail.svg';
 
 type Props = {
   onCategorySelect: (category: string | null) => void;
@@ -16,6 +22,8 @@ function CategoryFilter({ onCategorySelect, type }: Props) {
   const [categories, setCategories] = useState<string[]>([]);
   const [toggle, setToggle] = useState<string | null>(null);
   const icons = [beefIcon, breakfast, Chicken, dessert, goatIcon];
+  const drinkIcons = [shake, other, cocoa, ordinaryDrink, cocktail];
+
   useEffect(() => {
     const fetchCategories = async () => {
       let endpoint: string;
@@ -59,13 +67,14 @@ function CategoryFilter({ onCategorySelect, type }: Props) {
             onClick={ () => { onCategorySelect(null); setToggle(null); } }
           >
             {type === 'meals' && <img src={ allIcon } alt={ allIcon } />}
+            {type !== 'meals' && <img src={ allDrinks } alt={ allDrinks } />}
           </button>
         </div>
         <p>All</p>
       </div>
       {categories.map((category, index) => (
         <div key={ index }>
-          <div className={ category }>
+          <div className={ category } id={ `item-${index.toString()}` }>
             <button
               key={ category }
               data-testid={ `${category}-category-filter` }
@@ -73,6 +82,10 @@ function CategoryFilter({ onCategorySelect, type }: Props) {
               className={ category === toggle ? 'active' : '' }
             >
               {type === 'meals' && <img src={ icons[index] } alt={ category[index] } />}
+              {type !== 'meals' && <img
+                src={ drinkIcons[index] }
+                alt={ category[index] }
+              />}
             </button>
           </div>
           <p>{category}</p>
